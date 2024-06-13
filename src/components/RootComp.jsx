b668f42dfb0111018755595d775611bc518faf14
Moon-12 <ashwijanayak@gmail.com>
2024-06-13 17:35:42 -0500

    #3 admin and guest login functionality added

--- src/components/RootComp.jsx
+++ src/components/RootComp.jsx
@@ -4,25 +4,32 @@ import "./RootComp.css";
 import HeaderBar from "./HeaderBar/HeaderBar";
 import SideMenu from "./SideMenu/SideMenu";
 import { useEffect } from "react";
-import { auth } from "../firebase/firebaseConfig";
+import { auth, fireStoreDB } from "../firebase/firebaseConfig";
 import { onAuthStateChanged } from "firebase/auth";
 import { useDispatch } from "react-redux";
 import { setLogInResponse } from "../redux/slice/authSlice";
 import Footer from "./Footer/Footer";
+import { getDoc, doc } from "firebase/firestore";
 
 const RootComp = () => {
   const dispatch = useDispatch();
 
+  const getUserRole = async (user) => {
+    const docRef = doc(fireStoreDB, "user_profile", `${auth.currentUser.uid}`);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      return "admin";
+    } else if (user.isAnonymous) {
+      return "anonymous";
+    } else {
+      return "super_guest";
+    }
+  };
   useEffect(() => {
     //This is a firebase method,which gets called automatically when auth state changes
-    onAuthStateChanged(auth, (user) => {
+    onAuthStateChanged(auth, async (user) => {
       if (user) {
-        let role = "";
-        if (user.isAnonymous) {
-          role = "anonymous";
-        } else {
-          role = "super_guest";
-        }
+        const role = await getUserRole(user);
         dispatch(setLogInResponse({ uid: user.uid, role }));
       } else {
         dispatch(setLogInResponse(""));
--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const app = initializeApp(JSON.parse(process.env.REACT_APP_FIREBASE_TOKEN));
 export const auth = getAuth(app);
-
+export const fireStoreDB = getFirestore(app);
 export default app;
