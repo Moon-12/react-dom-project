617352119098e01fe894ee56ec60a2153fe7103f
Moon-12 <ashwijanayak@gmail.com>
2024-07-11 14:35:50 -0500

    await removed changed to then catch

--- src/components/Home/Projects/Easy/MillionCheckboxes/MillionCheckboxes.jsx
+++ src/components/Home/Projects/Easy/MillionCheckboxes/MillionCheckboxes.jsx
@@ -5,14 +5,12 @@ import { getDoc, doc, updateDoc, onSnapshot } from "firebase/firestore";
 
 const MillionCheckboxes = () => {
   const [checkboxMetadata, setCheckboxMetadata] = useState({});
-
+  const checkboxMetaDataDocRef = doc(
+    fireStoreDB,
+    "common",
+    "checkbox-metadata"
+  );
   useEffect(() => {
-    const checkboxMetaDataDocRef = doc(
-      fireStoreDB,
-      "common",
-      "checkbox-metadata"
-    );
-
     onSnapshot(checkboxMetaDataDocRef, (snapShot) => {
       if (snapShot.exists()) {
         setCheckboxMetadata(snapShot.data());
@@ -22,20 +20,18 @@ const MillionCheckboxes = () => {
 
   const handleChangeCheckbox = async (e) => {
     const targetCheckboxID = e.target.id;
-
-    const metaDataDocRef = doc(fireStoreDB, "common", "checkbox-metadata");
-    const docSnap = await getDoc(metaDataDocRef);
-
-    if (docSnap.data()) {
-      const isChecked = docSnap.data()[targetCheckboxID];
-      try {
-        await updateDoc(metaDataDocRef, {
-          [targetCheckboxID]: !isChecked,
-        });
-      } catch (error) {
+    getDoc(checkboxMetaDataDocRef)
+      .then((docSnap) => {
+        if (docSnap.data()) {
+          const isChecked = docSnap.data()[targetCheckboxID];
+          updateDoc(checkboxMetaDataDocRef, {
+            [targetCheckboxID]: !isChecked,
+          });
+        }
+      })
+      .catch((error) => {
         console.error("Error updating document: ", error);
-      }
-    }
+      });
   };
 
   return (
