4e3cceb9df5615c03bf2a60c57a78815d176090d
Moon-12 <ashwijanayak@gmail.com>
2024-08-07 10:49:59 -0400

    warning fixed

--- src/components/Home/ReactInterviewQuestions/FlashCardGame/FlashCard/ImagePopup/ImagePopup.jsx
+++ src/components/Home/ReactInterviewQuestions/FlashCardGame/FlashCard/ImagePopup/ImagePopup.jsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Modal from "@mui/material/Modal";
 import CardMedia from "@mui/material/CardMedia";
 
