f000eea36af657c88fd5814e43d3fd36bedfbbc0
Moon-12 <ashwijanayak@gmail.com>
2024-08-10 00:02:45 -0400

    new cat animation added

--- src/components/LandingPage/CatAnimation/CatAnimation.jsx
+++ src/components/LandingPage/CatAnimation/CatAnimation.jsx
@@ -1,6 +1,11 @@
 import "./CatAnimation.css";
+import catTyping from "../../../assets/cat-typing.webp";
 const CatAnimation = () => {
-  return <div id="gato"></div>;
+  return (
+    <div className="cat-typing">
+      <img src={catTyping} alt="" />
+    </div>
+  );
 };
 
 export default CatAnimation;
